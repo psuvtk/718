6fe6e539fbc927d832713f2441680adb6538e4a6
Kristoffer Song <psuvtk@gmail.com>
2018-11-05 22:23:27 +0800

    fix bug: bytearray_lremove

--- srrled_embedded/bytearray.c
+++ srrled_embedded/bytearray.c
@@ -1,6 +1,7 @@
 #include "./include/bytearray.h"
 
 void bytearray_init(bytearray_t *buf) {
+    printf("Func::bytearray_init\n");
     buf->data = NULL;
     buf->size = 0;
     buf->capacity = 0;
@@ -8,6 +9,7 @@ void bytearray_init(bytearray_t *buf) {
 
 
 void bytearray_destroy(bytearray_t *buf) {
+    printf("Func::bytearray_destroy\n");
     if (buf->data != NULL)
         free(buf->data);
     buf->size = 0;
@@ -16,7 +18,9 @@ void bytearray_destroy(bytearray_t *buf) {
 
 
 void bytearray_append(bytearray_t *buf, char *pdata, int len) {
-    if (buf->capacity == 0) {
+    printf("Func::bytearray_append\n");
+    if (buf->capacity == 0 && buf->data == NULL) {
+        printf("malloc new buf\n");
         buf->data = (char *)malloc(DEFAULT_BUF_LEN * sizeof(char));
         memcpy(buf->data, pdata, len);
         buf->size = len;
@@ -26,15 +30,14 @@ void bytearray_append(bytearray_t *buf, char *pdata, int len) {
 
     if (buf->capacity - buf->size >= len) {
         // 足以容纳
+        printf("memory enough\n");
         memcpy(&(buf->data)[buf->size], pdata, len);
         buf->size += len;
         return;
     } else {
         // 不足以容纳
-        int new_capacity = max(
-            buf->capacity * 2,
-            ((buf->capacity+len)/DEFAULT_BUF_LEN+1)*DEFAULT_BUF_LEN
-        );
+        printf("memory not enough, malloc new buf\n");
+        int new_capacity = buf->capacity * 2;
         char *new_buf = (char *)malloc(new_capacity * sizeof(char));
         memcpy(new_buf, buf->data, buf->size);
         memcpy(&new_buf[buf->size], pdata, len);
@@ -47,17 +50,20 @@ void bytearray_append(bytearray_t *buf, char *pdata, int len) {
 
 
 void bytearray_lremove(bytearray_t *buf, int len) {
-    memcpy(buf->data, &(buf->data)[len], len);
     buf->size -= len;
+    memcpy(buf->data, &(buf->data)[len], buf->size);
+
 }
 
 
 void bytearray_clear(bytearray_t *buf) {
+    printf("Func::bytearray_clear\n");
     buf->size = 0;
 }
 
 
 bool bytearray_startswith(const bytearray_t *buf, const char *pdata, int len) {
+    printf("Func::bytearray_startswith\n");
     if (buf->size < len)
         return false;
     return memcmp(buf->data, pdata, len) == 0;
@@ -65,10 +71,11 @@ bool bytearray_startswith(const bytearray_t *buf, const char *pdata, int len) {
 
 
 int bytearray_find(const bytearray_t *buf, const char *pdata, int len, int skip) {
+    printf("Func::bytearray_find\n");
     if (buf->size < len+skip) return -1;
     // FIXME: 是否有高效匹配的实现？
     for (int i = skip; i < buf->size-len; i++) {
-        if (memcpy(&(buf->data)[i], pdata, len) == 0)
+        if (memcmp(&(buf->data)[i], pdata, len) == 0) 
             return i;
     }
     return -1;
--- srrled_embedded/main.c
+++ srrled_embedded/main.c
@@ -3,14 +3,18 @@
 #include <string.h>
 
 #include "./include/radar_device.h"
+#include "./include/radar_packet.h"
 #include "./include/bytearray.h"
 
 #define MAX_BUF_LEN 1024
 #define SYNC_LEN 8
 
+
+void parse_packet(bytearray_t *buf, int next);
+
 int main (int argc, char *argv[]) {
     if (sensor_start()) {
-        printf("[+] sensor start success.");
+        printf("[+] sensor start success.\n");
     } else {
         perror("\nsensor start failed.");
         exit(-1);
@@ -18,30 +22,39 @@ int main (int argc, char *argv[]) {
 
     int fd;
     if (device_open(&fd)) {
-        printf("[+] device open success.");
+        printf("[+] device open success.\n");
     } else {
         perror("device open failed.");
         exit(-1);
     }
 
     bytearray_t framebuf;
+    bytearray_init(&framebuf);
+
     char recvbuf[MAX_BUF_LEN];
     const char sync[] = {0x02, 0x01, 0x04, 0x03, 0x06, 0x05, 0x08, 0x07};
 
     while (1) {
+        printf("\n\n---------------------------------------------");
         int num_recv = read(fd, recvbuf, MAX_BUF_LEN);
         bytearray_append(&framebuf, recvbuf, num_recv);
 
         if (bytearray_startswith(&framebuf, sync, SYNC_LEN)) {
+            printf("start with sync\n");
+            // for (int i=0; i < framebuf.size; i++) {
+            //         printf("%02x ", (framebuf.data)[i]);
+            //     }
             int next = bytearray_find(&framebuf, sync, 8, 8);
             if (next != -1) {
                 // deal_with_a_frame
-
+                parse_packet(&framebuf, next);
                 bytearray_lremove(&framebuf, next);
             } else {
+                printf("not find next \n");
                 continue;
             }
         } else {
+            printf("Not start with sync\n");
             int len = bytearray_find(&framebuf, sync, 8, 0);
             if (len == -1)
                 bytearray_clear(&framebuf);
@@ -50,5 +63,17 @@ int main (int argc, char *argv[]) {
         }
     }
 
+    bytearray_destroy(&framebuf);
     return 0;
+}
+
+
+void parse_packet(bytearray_t *buf, int next) {
+    packet_t packet;
+
+    packet_parse(&packet, buf->data, next);
+    printf("Frame NUmber: %d\n", packet.header.frameNumber);
+    // for (int i=0; i < framebuf.size; i++) {
+    //         printf("%02x ", (framebuf.data)[i]);
+    // }
 }
